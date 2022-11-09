5a71fb602f6602c069131672d7c243d61174759f
Hector Perez <hector.elliott.perez@gmail.com>
2022-11-09 10:04:43 -0800

    completed step 1 for control flow exercises

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,21 @@
+public class ControlFlowExercises {
+    public static void main(String args[])
+    {
+        // a
+        for(int i = 5; i <= 15; i++) {
+            System.out.println(i);
+        }
+
+        // b
+        for(int i = 100; i >= -10; i-=5) {
+            System.out.println(i);
+        }
+
+        // c
+        for(long i = 2; i < 1000000; i*=i) {
+            System.out.println(i);
+        }
+
+//        for(long i = 2;)
+    }
+}
