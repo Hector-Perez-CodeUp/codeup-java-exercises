04eb8d675cfafaed68e3fe299d71d4d1d16e8e2b
Hector Perez <hector.elliott.perez@gmail.com>
2022-11-17 12:53:23 -0800

    completed input test exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -6,13 +6,7 @@ public class Input {
 
     // Main
     public static void main(String[] args) {
-        Input input = new Input(new Scanner(System.in));
-        System.out.println(input.getString());
-        System.out.println(input.yesNo());
-        System.out.println(input.getInt(1, 10));
-        System.out.println(input.getInt());
-        System.out.println(input.getDouble(1, 10));
-        System.out.println(input.getDouble());
+
     }
 
     // Constructor
@@ -22,8 +16,7 @@ public class Input {
 
     public String getString() {
         System.out.println("Enter a string");
-        String userString = scanner.nextLine();
-        return userString;
+        return scanner.nextLine();
     }
 
     public boolean yesNo() {
